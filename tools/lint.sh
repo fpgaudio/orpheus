dc9ad4ba1da8aa5eab065b5c8783f7fc0f97e488
Marko Vejnovic <contact@markovejnovic.com>
2023-04-11 15:35:36 -0500

    Made linting automatically count failures so CI fails. Fixed missing compile_commands.json

--- tools/lint.sh
+++ tools/lint.sh
@@ -1,27 +1,28 @@
-#!/bin/sh
+#!/bin/bash
 
-# This is injected into find -exec sh -c ... {} \;, so it makes sense for $0
-# not to expand.
-# shellcheck disable=SC2016
-C_LINT_CMD='echo Linting $0.; clang-format -Werror --dry-run $0; clang-tidy $0'
+shopt -s nullglob
+
+declare -i LINT_FAILED=0
 
 lint_dir() {
-    find "$1" -type f '(' \
-        -name "*.cpp" \
-        -o -name "*.c" \
-        -o -name "*.hpp" \
-        -o -name "*.h" \
-    ')' \
-        -exec sh -c "$C_LINT_CMD" {} \;
+    for x in "$1"/*.{c,cpp,h,hpp}; do
+        echo "Linting $x."
+        clang-format -Werror --dry-run "$x"
+        LINT_FAILED+=$?
+        clang-tidy "$x"
+        LINT_FAILED+=$?
+    done
 
-    find "$1" -type f '(' \
-        -name "*.sh" \
-        -o -name "*.bash" \
-    ')' \
-        -exec sh -c 'echo Linting $0.; shellcheck $0' {} \;
+    for x in "$1"/*.{sh,bash}; do
+        echo "Linting shell-script $x."
+        shellcheck "$x"
+        LINT_FAILED+=$?
+    done
 }
 
 cd "${MESON_SOURCE_ROOT}" || exit 1;
 lint_dir src
 lint_dir include
 lint_dir tools
+
+exit "$LINT_FAILED"
