5e48dfd93db14606cca55b4a3d777ad38c655ff4
Marko Vejnovic <contact@markovejnovic.com>
2023-04-11 23:02:51 -0500

    Attempted to add the sin library

--- src/orpheus.cpp
+++ src/orpheus.cpp
@@ -5,6 +5,7 @@
 #include <numbers>
 #include <numeric>
 #include <type_traits>
+#include "intmath.hpp"
 
 namespace Orpheus {
 
@@ -22,24 +23,10 @@ void Attenuator::setAtten(AttenFactor attenuation) {
 }
 
 auto SineSource::operator()() const -> Engine::QuantType {
-  // TODO(markovejnovic): "SineSource uses floating point math::sin. This should
-  // get rewritten."
   // TODO(markovejnovic): SineSource performs a possibly expensive operation
   // that does not get memoized. Wasted.
 
-  const auto tickCount = getEngine().getTickCount();
-  const auto samplePeriodNs = getEngine().getSamplePeriodNs();
-  const auto samplePeriodS = samplePeriodNs * 1e-9;
-  const auto cTime = static_cast<float>(tickCount * samplePeriodNs) * 1e-9;
-
-  const auto frequency =
-      1.0F / static_cast<float>(getPeriodTicks() * samplePeriodS);
-  const auto ampl =
-      static_cast<float>(std::numeric_limits<Engine::QuantType>::max());
-  const auto out = static_cast<float>(
-      ampl * std::sin(2 * std::numbers::pi * frequency * cTime));
-
-  return static_cast<Engine::QuantType>(out);
+  return static_cast<Engine::QuantType>(Orpheus::Math::Approx::sin(getEngine().getTickCount()));
 }
 
 } // namespace Graph
